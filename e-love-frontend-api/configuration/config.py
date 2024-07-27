45947eb0e193c13a25cb25f0f3c7abe08989340e
paketoff <kekisreal228@gmail.com>
2024-07-28 00:06:42 +0200

    Add basic environment handling

--- e-love-frontend-api/configuration/config.py
+++ e-love-frontend-api/configuration/config.py
@@ -1,17 +1,19 @@
-# import os
-# from pydantic_settings import BaseSettings
-# from dotenv import load_dotenv
+# config.py
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
-# load_dotenv()
+# Loading .env file
+load_dotenv(os.getenv('APP_ENV_PATH'))
+print(f"Loaded .env path: {os.getenv('APP_ENV_PATH')}")
 
-# class Settings(BaseSettings):
-#     API_V1_STR: str = "/api/v1"
-#     PROJECT_NAME: str = "E-Love Frontend API"
-#     BACKEND_CORS_ORIGINS: str = "*"
-#     DATABASE_URI: str = os.getenv("DATABASE_URI")
-#     SECRET_KEY: str = os.getenv("SECRET_KEY")
+class Settings(BaseSettings):
+    app_name: str
+    app_version: str
+    app_running_env: str
+    greeting_message: str
 
-#     class Config:
-#         env_file = ".env"
+    class Config:
+        env_file = os.getenv('APP_ENV_PATH')
 
-# settings = Settings()
+settings = Settings()
--- e-love-frontend-api/main.py
+++ e-love-frontend-api/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from configuration.config import settings
 
-app = FastAPI()
-
-# @app.on_event("startup")
-# async def startup_event():
-#     print(f"Database URL: {settings.DATABASE_URL}")
-#     print(f"Secret Key: {settings.SECRET_KEY}")
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
 
 @app.get("/hello")
 async def hello():
     return {"message": "Hello, World!"}
+
+@app.get("/config-info")
+async def config_info():
+    return {
+        "app_name": settings.app_name,
+        "app_version": settings.app_version,
+        "app_running_env": settings.app_running_env,
+        "greeting_message": settings.greeting_message,
+    }
