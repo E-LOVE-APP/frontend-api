03578df6a61ce71e763d6d8127d3ae79412ef870
paketoff <kekisreal228@gmail.com>
2024-09-12 23:53:28 +0200

    feat(UC-4-alembic-part): Remove wrong revision, add correct one. Add additional base-model corrections

--- e-love-frontend-api/core/db/models/base.py
+++ e-love-frontend-api/core/db/models/base.py
@@ -1,22 +1,18 @@
 # Module that represents an abstract base model
 
 import uuid
-from sqlalchemy import Column, DateTime, func
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import as_declarative, declared_attr
+from sqlalchemy import Column, DateTime, func, String
+from sqlalchemy.orm import declared_attr
 from configuration.database import Base
 
 
 """ Basic datatable model  """
 
 
-@as_declarative()
-class BaseModel:
+class BaseModel(Base):
     __abstract__ = True
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False
-    )
+    id = Column(String(36), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
