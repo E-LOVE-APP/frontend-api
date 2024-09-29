59d827bd91231b44d1e1e50f704d81882df3eb78
paketoff <kekisreal228@gmail.com>
2024-09-29 03:14:25 +0200

    impr(UC-11): Add very-basic user service;

---
+++ e-love-frontend-api/core/schemas/users/user_schema.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email: Optional[str]
+    password: Optional[str]
+
+
+class UserOutput(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+
+    class Config:
+        orm_mode = True
