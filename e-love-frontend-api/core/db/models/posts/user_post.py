3cfac73309688dabb42c38cf233baff384dd8bac
Neepward <s17102002s92@gmail.com>
2024-11-27 00:27:53 +0200

    Improvement(UC-29): Add selectin and new created_at field

--- e-love-frontend-api/core/db/models/posts/user_post.py
+++ e-love-frontend-api/core/db/models/posts/user_post.py
@@ -1,5 +1,5 @@
 # type: ignore
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, DateTime, ForeignKey, String, func
 from sqlalchemy.orm import relationship
 
 from core.db.models.intermediate_models.posts_categories import posts_categories_table
@@ -14,7 +14,8 @@ class UserPost(BaseModel):
     post_title = Column(String(250), nullable=False)
     post_descr = Column(String(1000), nullable=False)
     user_id = Column(ForeignKey("user.id"), nullable=False)
-    user = relationship("User", back_populates="posts")
+    user = relationship("User", back_populates="posts", lazy="selectin")
+    created_at = Column(DateTime, default=func.now())
 
     category = relationship(
         "Categories", secondary=posts_categories_table, back_populates="posts", lazy="selectin"
