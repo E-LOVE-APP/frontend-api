68c9a655212ae978d69e6a6bec713c453d32bc9d
paketoff <kekisreal228@gmail.com>
2024-11-26 13:11:04 +0100

    feat(UC-32): Add ConnectionManager endpoint

---
+++ e-love-frontend-api/api/conn_manager/conn_manager.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect
+
+from auth.security import authenticator
+
+router = APIRouter(
+    prefix="/chat",
+)
+
+
+@router.websocket(
+    "/",
+    # response_model=
+    response={
+        1000: {
+            "description": "Message has been delivered successfully",
+            # "model": MessageOutput,
+        },
+        1011: {
+            "description": "Internal server error.",
+            # "model": WebSocketInternalError,
+        },
+    },
+    tags=["Messages", "Chat"],
+    dependencies=[
+        Depends(authenticator.authenticate),
+    ],
+)
+async def create_wb_connection(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text({data})
+    except WebSocketDisconnect:
+        print("Client has been disconnected")
