1f7c47afde58c82588d8e44f15f700fa41f525e5
Neepward <s17102002s92@gmail.com>
2024-12-09 03:35:38 +0200

    feat(UC-34): Set back UUID field

--- e-love-frontend-api/core/db/models/users/user_images.py
+++ e-love-frontend-api/core/db/models/users/user_images.py
@@ -11,6 +11,6 @@ class UserImages(BaseModel):
     __tablename__ = "user_images"
 
     img_url: Column[str] = Column(String(124), nullable=False)
-    user_id: Column[str] = Column(ForeignKey("user.id"), nullable=False)
+    user_id: Column[UUID] = Column(ForeignKey("user.id"), nullable=False)
     # TODO: fix list of users
     user: Mapped[List["User"]] = relationship("User", back_populates="image", uselist=True)
